e512b463d5a35adf1b4d5574f7090ecf5e04ebb5
Olga Dudina <oskina.os.spb@gmail.com>
2021-07-22 02:21:00 +0300

    add HUD

--- AviasalesTestApp/AviasalesTestApp/NetworkService/NetworkWorker.swift
+++ AviasalesTestApp/AviasalesTestApp/NetworkService/NetworkWorker.swift
@@ -6,6 +6,7 @@
 //
 
 import Alamofire
+import SVProgressHUD
 
 struct RequestModel {
     let url: String
@@ -18,12 +19,18 @@ class NetworkWorker {
                              of: T.Type,
                              completion: @escaping ((Result<T.DomainType, AFError>) -> Void)) {
         AF.cancelAllRequests()
+        showHUD()
         
         AF.request(requestModel.url,
                    method: requestModel.method,
                    parameters: requestModel.params)
             .validate()
             .responseDecodable(of: T.self) { response in
+                if let error = response.error,
+                      error.isExplicitlyCancelledError { return }
+                
+                self.dismissHUD()
+                
                 switch response.result {
                 case .success(let data):
                     DispatchQueue.main.async {
@@ -35,5 +42,19 @@ class NetworkWorker {
                     }
                 }
             }
+    }
+}
+
+extension NetworkWorker {
+    func showHUD() {
+        DispatchQueue.main.async {
+            SVProgressHUD.show()
+        }
+    }
+    
+    func dismissHUD() {
+        DispatchQueue.main.async {
+            SVProgressHUD.dismiss()
+        }
     }
 }
